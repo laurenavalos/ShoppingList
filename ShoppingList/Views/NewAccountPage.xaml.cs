a747087bcbc22159d0639193d33d0b5ad48c59a7
lauren <800077081@fvtc.edu>
2026-05-15 13:28:26 -0500

    started week 15

--- ShoppingList/Views/NewAccountPage.xaml.cs
+++ ShoppingList/Views/NewAccountPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ShoppingList.Models;
 
 namespace ShoppingList.Views;
 
@@ -14,8 +16,17 @@ public partial class NewAccountPage : ContentPage
         Title = "Create New Account";
     }
 
-    private void CreateAccount_OnClicked(object sender, EventArgs e)
+    async void CreateAccount_OnClicked(object sender, EventArgs e)
     {
+        //api
+        var data = JsonConvert.SerializeObject(new userAccount(txtUser.Text, txtPassword1.Text, txtEmail.Text));
+        var client = new HttpClient();
+        var response = await client.PostAsync(new Uri("http://joewetzel.com/fvtc/account/createuser"),
+            new StringContent(data, Encoding.UTF8, "application/json"));
+        var AccountStatus = response.Content.ReadAsStringAsync().Result;
+        
+        AccountStatus = AccountStatus;
+        
         App.SessionKey = "aaa";
         Navigation.PopModalAsync();
     }
