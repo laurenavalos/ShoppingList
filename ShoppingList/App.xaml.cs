ff5b40c1b8a814f8742e4974fd069e80d910159d
lauren <800077081@fvtc.edu>
2026-05-15 12:50:26 -0500

    fixed changes for week 14

--- ShoppingList/App.xaml.cs
+++ ShoppingList/App.xaml.cs
@@ -1,11 +1,13 @@
-﻿namespace ShoppingList;
+﻿using ShoppingList.Views;
+namespace ShoppingList;
 
 public partial class App : Application
 {
+    public static string SessionKey = "";
     public App()
     {
         InitializeComponent();
 
-        MainPage = new AppShell();
+        MainPage = new NavigationPage(new MainPage());
     }
 }
--- ShoppingList/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShoppingList;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
